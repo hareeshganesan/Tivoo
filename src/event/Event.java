761b8dc838a4b659bf6418ac1e5f770d29acc0ba
Alexander Bruce <ajbruce60@gmail.com>
2012-02-11 14:34:38 -0500

    Basic start

---
+++ src/event/Event.java
@@ -0,0 +1,46 @@
+package event;
+
+import java.util.Date;
+
+
+public class Event
+{
+    private String myTitle;
+    private String mySummary;
+    private Date myStart;
+    private Date myEnd;
+
+
+    public Event (String title, String sum, Date start, Date end)
+    {
+        myTitle = title;
+        mySummary = sum;
+        myStart = start;
+        myEnd = end;
+    }
+
+
+    public String getMyTitle ()
+    {
+        return myTitle;
+    }
+
+
+    public String getMySummary ()
+    {
+        return mySummary;
+    }
+
+
+    public Date getMyStart ()
+    {
+        return myStart;
+    }
+
+
+    public Date getMyEnd ()
+    {
+        return myEnd;
+    }
+
+}
