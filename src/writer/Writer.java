697f003a08a0ee07e16ee22a17d9df91e0685e44
Hareesh Ganesan <hareesh.ganesan@gmail.com>
2012-02-14 16:06:26 -0500

    fixed merge conflicts

--- src/writer/Writer.java
+++ src/writer/Writer.java
@@ -1,5 +1,55 @@
 package writer;
 
-public abstract class Writer {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import com.hp.gagawa.java.elements.Body;
+import com.hp.gagawa.java.elements.H1;
+import com.hp.gagawa.java.elements.Head;
+import com.hp.gagawa.java.elements.Html;
+import com.hp.gagawa.java.elements.Text;
+import com.hp.gagawa.java.elements.Title;
+
+public abstract class Writer
+{
+
+    protected String myName;
+    
+    protected void write (Html html, String filename)
+    {
+        try
+        {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+            bw.write(html.write());
+            bw.flush();
+            bw.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("error");
+        }
+    }
+
+    protected Html initializeHTMLDocument ()
+    {
+        Html html = new Html();
+        Head head = new Head();
+    
+        html.appendChild(head);
+    
+        Title title = new Title();
+        title.appendChild(new Text(myName+" Page"));
+        head.appendChild(title);
+    
+        Body body = new Body();
+    
+        html.appendChild(body);
+    
+        H1 h1 = new H1();
+        h1.appendChild(new Text(myName));
+        body.appendChild(h1);
+    
+        return html;
+    }
     
 }
