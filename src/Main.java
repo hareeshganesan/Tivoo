59f21ff9eda7752866b2b42296097c6a647654bf
kh156 <kuanghan.kevin@gmail.com>
2012-02-16 16:49:21 -0500

    template commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    public static void main (String[] args) {        
+    public static void main (String[] args) {    
         TivooSystem s = new TivooSystem();
         s.loadFile("./DukeBasketBall.xml");
         s.filterByKeyword("vs");
