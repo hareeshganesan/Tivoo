5ac2f1ba813c748e53f42e3d59e3cef69f09baf1
Hareesh Ganesan <hareesh.ganesan@gmail.com>
2012-02-15 23:34:23 -0500

    fixing source control...

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         s.filterByKeyword("vs");
         s.filterByTimeFrame("11/21/11 6:30 PM", "01/21/12 1:00 PM");        
         s.outputSummary("./html/summary.html");
-        s.outputDetails("./html/details.html");
+        s.outputDetails("./html/event");
     }
 
 }
--- src/writer/SummaryWriter.java
+++ src/writer/SummaryWriter.java
@@ -10,7 +10,12 @@
 package writer;
 
 import event.Event;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import com.hp.gagawa.java.elements.*;
 
 public class SummaryWriter extends Writer
@@ -28,22 +33,47 @@ public class SummaryWriter extends Writer
 
         Html html = initializeHTMLDocument();
 
-        Table table = new Table();
-        /**
-         * For each element in the events list, add a td to the table
-         */
-        for(Event event : events){
-            Tr event_format = new Tr();
-            event_format.appendChild((new Td()).appendChild(new Text(event.getMyTitle())));
-            event_format.appendChild((new Td()).appendChild(new Text(event.getMyStart().toString())));
-            event_format.appendChild((new Td()).appendChild(new Text(event.getMyEnd().toString())));   
-            
-            table.appendChild((event_format));
-        }
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        HashMap<Integer, ArrayList<Event>> eventsByDate = groupByDate(events);
+
         
-        html.appendChild(table);
+        for(int i=0; i<7; i++){
+            ArrayList<Event> day = eventsByDate.get(i);
+            Table table = new Table();
+            for(Event event : day){
+                Tr event_format = new Tr();
+                A link = new A();
+                link.setHref("event"+events.indexOf(event)+".html");
+                link.appendChild(new Text(event.getMyTitle()));
+                
+                event_format.appendChild((new Td()).appendChild(link));
+                event_format.appendChild((new Td()).appendChild(new Text(event.getMyStart().toString())));
+                event_format.appendChild((new Td()).appendChild(new Text(event.getMyEnd().toString())));  
+                
+                table.appendChild(event_format);
+            }
+            Text dayOfTheWeek = new Text(days[i]);
+            html.appendChild(new H2().appendChild(dayOfTheWeek));
+            html.appendChild(table);
+        }
+                 
+                
+        int x = Calendar.SUNDAY;
         
         write(html, filename);
 
+    }
+    
+    private HashMap<Integer, ArrayList<Event>> groupByDate(List<Event> events){
+        HashMap<Integer, ArrayList<Event>> dateSet = new HashMap<Integer, ArrayList<Event>>();
+        for(int i=0; i<7; i++)
+            dateSet.put(i, new ArrayList<Event>());
+        
+        for(Event event : events){
+            dateSet.get(event.getMyStart().getDay()).add(event);
+        }
+        
+        return dateSet;
+            
     }
 }
