8b7e47d904e907d939261c8d675b2681fd081e70
Hareesh Ganesan <hareesh.ganesan@gmail.com>
2012-02-22 17:39:30 -0500

    fixed time frame edits

--- src/filter/FilterByTimeFrame.java
+++ src/filter/FilterByTimeFrame.java
@@ -3,24 +3,31 @@ package filter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
 import java.util.*;
 import event.Event;
 import exception.*;
 
-public class FilterByTimeFrame extends Filter{
+
+public class FilterByTimeFrame extends Filter
+{
 
     private static String defaultDateFormatString = "MM/dd/yy hh:mm:ss a";
-    private static DateFormat defaultDateFormat = new SimpleDateFormat(defaultDateFormatString);
+    private static DateFormat defaultDateFormat =
+            new SimpleDateFormat(defaultDateFormatString);
 
     private Date myStartTime, myEndTime;
 
-    public FilterByTimeFrame (String startTime, String endTime) {
+
+    public FilterByTimeFrame (String startTime, String endTime)
+    {
         Date start, end;
-        try {
+        try
+        {
             start = defaultDateFormat.parse(startTime);
             end = defaultDateFormat.parse(endTime);
-        } catch (ParseException e) {
+        }
+        catch (ParseException e)
+        {
             throw new TivooIllegalDateFormat();
         }
         myOriginalList = new ArrayList<Event>();
@@ -29,27 +36,49 @@ public class FilterByTimeFrame extends Filter{
         myEndTime = end;
     }
 
+
     @Override
-    public void filter(List<Event> list) {
+    public void filter (List<Event> list)
+    {
         myOriginalList = list;
         myFilteredList = new ArrayList<Event>();
-        for (Event entry: myOriginalList) {
-            if (entry.isWithinTimeFrame(myStartTime, myEndTime)) {
+        for (Event entry : myOriginalList)
+        {
+            if (isWithinTimeFrame(entry))
+            {
                 myFilteredList.add(entry);
             }
         }
         nextFilter();
-    }    
-    
-    public static String getDefaultDateFormat() {
+    }
+
+
+    public static String getDefaultDateFormat ()
+    {
         return defaultDateFormatString;
     }
-    
 
-    public boolean isWithinTimeFrame(Date startTime, Date endTime) {
-        Date eventStartTime=(Date) myField.get(Type.START);
-        Date eventEndTime= (Date) myField.get(Type.END);
-        return (eventStartTime.before(endTime) && eventEndTime.after(startTime));
+
+    public boolean isWithinTimeFrame (Event event)
+    {
+        DateFormat format = new SimpleDateFormat(Event.dateFormat);
+
+        Date eventStartTime;
+        Date eventEndTime;
+        try
+        {
+            eventStartTime = format.parse(event.get("startDate"));
+            eventEndTime = format.parse(event.get("endDate"));
+            return (eventStartTime.before(myStartTime) && eventEndTime.after(myEndTime));
+        }
+        catch (ParseException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return false;
+
     }
 
 }
