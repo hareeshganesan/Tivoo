17280b2944a1adad72956f46f24ac158f527cbe1
kh156 <kuanghan.kevin@gmail.com>
2012-02-27 02:17:09 -0500

    improved FilterByKeywordList

---
+++ src/filter/FilterByKeywordList.java
@@ -0,0 +1,36 @@
+package filter;
+
+import java.util.List;
+import event.Event;
+
+
+public class FilterByKeywordList extends FilterDecorator
+{
+
+    private List<String> myKeywordList;
+
+
+    public FilterByKeywordList (List<String> keywordList)
+    {
+        super();
+        myKeywordList = keywordList;
+    }
+
+
+    @Override
+    public void filter (List<Event> list)
+    {
+        List<Event> decoratedList = decoratedFilterWork(list);
+        for (Event entry : decoratedList)
+        {
+            for (String keyword:myKeywordList)
+            {
+                if (entry.containsKeyword(keyword))
+                {
+                    myFilteredList.add(entry);
+                    break;
+                }
+            }
+        }
+    }
+}
